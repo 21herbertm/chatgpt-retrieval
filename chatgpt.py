330cce161aeac61c677655eac85eba4cba83f713
21herbertm <53823046+21herbertm@users.noreply.github.com>
2023-11-18 15:33:28 -0500

    updated nutritionist

--- chatgpt.py
+++ chatgpt.py
@@ -2,13 +2,12 @@ import os
 import sys
 
 import openai
-from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.document_loaders import DirectoryLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
-from langchain.llms import OpenAI
 from langchain.vectorstores import Chroma
 
 import constants
@@ -20,33 +19,41 @@ PERSIST = False
 
 query = None
 if len(sys.argv) > 1:
-  query = sys.argv[1]
+    query = sys.argv[1]
 
-if PERSIST and os.path.exists("persist"):
-  print("Reusing index...\n")
-  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
-  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
-else:
-  #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
-  loader = DirectoryLoader("data/")
-  if PERSIST:
-    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
-  else:
-    index = VectorstoreIndexCreator().from_loaders([loader])
+try:
+    if PERSIST and os.path.exists("persist"):
+        print("Reusing index...\n")
+        vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+        index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+    else:
+        loader = DirectoryLoader("data/")
+        print("Loading documents...")
+        docs = loader.load()  # Check what documents are loaded
+        print(f"Loaded {len(docs)} documents.")
+        if PERSIST:
+            index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory": "persist"}).from_loaders([loader])
+        else:
+            index = VectorstoreIndexCreator().from_loaders([loader])
+        print("Index created.")
 
-chain = ConversationalRetrievalChain.from_llm(
-  llm=ChatOpenAI(model="gpt-3.5-turbo"),
-  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-)
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model="gpt-3.5-turbo"),
+        retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+    )
 
-chat_history = []
-while True:
-  if not query:
-    query = input("Prompt: ")
-  if query in ['quit', 'q', 'exit']:
-    sys.exit()
-  result = chain({"question": query, "chat_history": chat_history})
-  print(result['answer'])
+    chat_history = []
+    while True:
+        if not query:
+            query = input("Prompt: ")
+        print(f"Query: {query}")  # Print out the query to debug
+        if query.lower() in ['quit', 'q', 'exit']:
+            break
+        result = chain({"question": query, "chat_history": chat_history})
+        print(f"Response: {result['answer']}")  # Print out the answer to debug
 
-  chat_history.append((query, result['answer']))
-  query = None
+        chat_history.append((query, result['answer']))
+        query = None
+
+except Exception as e:
+    print(f"An error occurred: {e}")
